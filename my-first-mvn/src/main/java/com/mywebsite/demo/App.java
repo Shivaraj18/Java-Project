8dd1f6fcb612b946013cf8c12699a6c11b178d5b
Shivaraj <shiva.navalgund@gmail.com>
2019-01-12 08:16:59 +0000

    My Maven Project

---
+++ my-first-mvn/src/main/java/com/mywebsite/demo/App.java
@@ -0,0 +1,17 @@
+package com.mywebsite.demo;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	     for(int i=0;i<=10;i++)
+		{
+			System.out.println(i+"...Hello World!");
+		}
+		System.out.println("Program Ended..!");
+    }
+
+}
